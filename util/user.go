61d01f09406bca5e895559e266c775b02c0ba9e6
lbaf23 <2282480289@qq.com>
2021-08-10 23:00:08 +0800

    refactor: use function to judge user type

--- util/user.go
+++ util/user.go
@@ -5,3 +5,10 @@ import "github.com/casdoor/casdoor-go-sdk/auth"
 func GetUserId(claims *auth.Claims) (id string) {
 	return claims.Name
 }
+
+func IsStudent(claims *auth.Claims) (b bool) {
+	return claims.Tag == "student"
+}
+func IsTeacher(claims *auth.Claims) (b bool) {
+	return claims.Tag == "teacher"
+}
