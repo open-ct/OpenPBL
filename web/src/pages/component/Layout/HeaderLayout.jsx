b44a7c77570d2caf5ccff7e0554ca5270d81002a
lbaf23 <2282480289@qq.com>
2021-08-11 09:39:50 +0800

    refactor: get images from cdn

--- web/src/pages/component/Layout/HeaderLayout.jsx
+++ web/src/pages/component/Layout/HeaderLayout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {Avatar, Badge, Button, Col, Dropdown, Layout, Menu, Row, Tag, Image} from "antd";
 import {Link, Redirect, Route, Switch} from "react-router-dom";
 import {BellOutlined, LogoutOutlined, SettingOutlined} from '@ant-design/icons';
-import {connect} from "react-redux";
 
 import * as Auth from "../../User/Auth/Auth"
 import AuthApi from "../../../api/AuthApi"
@@ -26,7 +25,6 @@ class HeaderLayout extends React.Component {
     current: 'home',
     account: null,
     messageCount: 0,
-    menu: 'home'
   }
 
   componentDidMount() {
@@ -36,34 +34,19 @@ class HeaderLayout extends React.Component {
           this.setState({
             account: res.data.data
           })
-          this.props.setUserType(res.data.data.tag)
+          localStorage.setItem("type", res.data.data.tag)
         } else {
-          this.props.setUserType("")
+          localStorage.setItem("type", "")
         }
       })
-      .catch((e) => {console.log(e)})
-
-    this.changeMenu()
-  }
-
-  changeMenu = (e) => {
-    if (e !== undefined ) {
-      this.setState({menu: e.key})
-      return
-    }
-    const p = this.props.location.pathname
-    console.log(p)
-    if (p.startsWith('/home')) {
-      this.setState({menu: 'home'})
-    } else if (p.startsWith("/my-project")) {
-      this.setState({menu: 'my-project'})
-    } else if (p.startsWith("/public-project")) {
-      this.setState({menu: 'public-project'})
-    }
+      .catch((e) => {
+        console.log(e)
+      })
   }
 
   handleRightDropdownClick(e) {
     let account = this.state.account;
+    console.log(account)
     if (e.key === 'my-account') {
       window.open(Auth.getMyProfileUrl(account));
     } else if (e.key === 'logout') {
@@ -73,11 +56,13 @@ class HeaderLayout extends React.Component {
             this.setState({
               account: null
             })
-            this.props.setUserType("")
+            localStorage.setItem("type", "")
             window.location.href = '/'
           }
         })
-        .catch(e => {console.log(e)})
+        .catch(e => {
+          console.log(e)
+        })
     }
   }
 
@@ -95,7 +80,7 @@ class HeaderLayout extends React.Component {
       </Menu>
     )
     return (
-      <Dropdown overlay={menu} placement="bottomRight" trigger="click">
+      <Dropdown overlay={menu} placement="bottomRight">
         <div style={{cursor: 'pointer'}}>
           <Avatar size="large" src={this.state.account.avatar}/>&nbsp;
           <span>{this.state.account.name}</span>
@@ -114,15 +99,6 @@ class HeaderLayout extends React.Component {
     } else {
       return (
         <>
-        <span style={{float: 'left'}}>
-          {this.props.userType === 'student' ? <Tag>学生</Tag> :
-            <>
-              {this.props.userType === 'teacher' ? <Tag>教师</Tag> :
-                <Tag>未知角色</Tag>
-              }
-            </>
-          }
-        </span>
           {this.renderRightDropdown()}
         </>
       )
@@ -130,25 +106,18 @@ class HeaderLayout extends React.Component {
   }
 
   render() {
-    const {menu, messageCount} = this.state;
+    const {current, messageCount} = this.state;
     return (
       <Layout style={{minHeight: '100vh', textAlign: 'left'}}>
-        <Layout.Header style={{backgroundColor: 'white', paddingLeft: '4px', paddingRight: '4px'}}>
+        <Layout.Header style={{backgroundColor: 'white'}}>
           <Row>
-            <Col xxl={14} xl={10} lg={8} md={6} sm={6} xs={10}>
+            <Col xxl={15} xl={11} lg={8} md={6} sm={6} xs={10}>
               <Link to="/home">
                 <Image height={60} style={{margin: '2px'}} src={logo} preview={false}/>
               </Link>
             </Col>
-            <Col xxl={6} xl={10} lg={10} md={10} sm={10} xs={3}>
-              <Menu
-                theme="light"
-                mode="horizontal"
-                style={{border: 0}}
-                defaultSelectedKeys={['home']}
-                selectedKeys={[menu]}
-                onClick={e=>this.changeMenu(e)}
-              >
+            <Col xxl={6} xl={10} lg={12} md={14} sm={12} xs={6}>
+              <Menu theme="light" mode="horizontal" defaultSelectedKeys={[current]} style={{border: 0}}>
                 <Menu.Item key="home">
                   <Link to="/home">
                     首页
@@ -171,7 +140,7 @@ class HeaderLayout extends React.Component {
                 </Menu.Item>
               </Menu>
             </Col>
-            <Col xxl={4} xl={4} lg={6} md={8} sm={8} xs={11}>
+            <Col xxl={3} xl={3} lg={4} md={4} sm={6} xs={8}>
               {
                 <>
                   <span style={{float: 'left', marginRight: '20px'}}>
@@ -222,23 +191,4 @@ class HeaderLayout extends React.Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    userType: state.get("userType").get("userType")
-  }
-}
-
-const setType = (userType) => {
-  return {
-    type: 'set',
-    userType: userType
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    setUserType: (userType)=>dispatch(setType(userType))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(HeaderLayout);
+export default HeaderLayout;
